17e544b683ac69699f0f4e901f18f151f08e02e7
Marcos Grala <marcosgrala@live.com>
2018-07-06 18:31:40 -0300

    Replace norm and cross, change the Dataframe use

--- temporal.py
+++ temporal.py
@@ -8,6 +8,21 @@ import scipy.fftpack
 import pandas as pd
 import glob
 from scipy import signal
+import time
+
+
+def normD(a):
+    norm = 0
+    for i in range(3):
+        norm += a[i]*a[i]
+    return np.sqrt(norm)
+
+def crossD(a,b):
+    cross = [0]*3
+    cross[0] = a[1]*b[2]+a[2]*b[1]
+    cross[1] = a[2]*b[0]+a[0]*b[2]
+    cross[2] = a[0]*b[1]+a[1]*b[2]
+    return cross
 
 class DifCoeficient(object):
     """
@@ -34,23 +49,26 @@ class DifCoeficient(object):
 
     def __init__(self, filename, RVal, TimeStep):
         self.name = filename # all the directory
-        self.radius = RVal # raduial distance
+        self.radius = RVal # radial distance
         self.tStep = TimeStep # Simulation time step
-
+        self.len = 0 #number of timesteps
 
     def read(self):
-    	#read from file
+        #read from file
         # faz a leitura de todos os arquivos da pasta
         files = glob.glob(self.name)
         files.sort() # sort the filenames
-        self.inputArray = []
+        self.len = len(files)
+        self.inputArray = [0]*self.len
         # armazena em uma lista os data frames para cada passo temporal
+        j = 0
         for i in files:
-	    # data frame temporário contedo todos os dados
+        # data frame temporário contedo todos os dados
             Array = pd.DataFrame(np.loadtxt(i), columns=['x', 'y', 'R', 'phi', 'ex', 'ey', 'ez', 'bx', 'by', 'bz'])
-	    # lista de dataframes somente com os dados para o R selecionado
-            self.inputArray.append(Array[Array['R'] == self.radius])
-
+             # lista de dataframes somente com os dados para o R selecionado
+            #self.inputArray.append(Array[Array['R'] == self.radius])
+            self.inputArray[j] = Array[Array['R'] == self.radius]
+            j+=1 
 
     # convert the coordinate system from gse to field aligned system
     def rotate_field_fac(self):
@@ -61,12 +79,13 @@ class DifCoeficient(object):
         '''
 
 
+        #self.fields = [0]*self.len
         self.fields = []
-	# loop in the fataframes od the timesteps
+    # loop in the fataframes od the timesteps
         for k in self.inputArray:
-	    # temporary data frame for the new rotate fields
-	    ## mantive os mesmos nomes das variáveis que estavam na rotina do vitor. v1p, v1a, v1r, são e_paralelo, e_azimutal(ephi), e_radial
-            tempFields = pd.DataFrame(np.zeros((len(k['x']), 8)),columns=['bp', 'ba', 'br', 'v1p', 'v1a', 'v1r', 'b_fac', 'b_orig'])
+        # temporary data frame for the new rotate fields
+        ## mantive os mesmos nomes das variáveis que estavam na rotina do vitor. v1p, v1a, v1r, são e_paralelo, e_azimutal(ephi), e_radial
+            #  = pd.DataFrame(np.zeros((len(k['x']), 8)),columns=['bp', 'ba', 'br', 'v1p', 'v1a', 'v1r', 'b_fac', 'b_orig'])
             # Extract the original data components
             x = k['x'].values 
             y = k['y'].values
@@ -77,29 +96,35 @@ class DifCoeficient(object):
             bx = k['bx'].values
             by = k['by'].values
             bz = k['bz'].values
-		
-	    # 'p', 'a', e, 'r', denotam as variáveis na direção paralela, azimutal(phi) e radial
-	
+            tempFields = np.zeros((len(k['x']), 8))
+        
+            # 'p', 'a', e, 'r', denotam as variáveis na direção paralela, azimutal(phi) e radial
             for i in range(0,len(x)):
                 r  = [x[i], y[i], z[i]] / np.sqrt(x[i] * x[i] + y[i] * y[i] + z[i] * z[i])
-		# mais uma ves, mantive os mesmos nomes
-		## esses índices referem-se aos vetores unitários nessas direçes.
+                # mais uma ves, mantive os mesmos nomes
+                ## esses índices referem-se aos vetores unitários nessas direçes.
                 ep = [bx[i], by[i], bz[i]] / np.sqrt(bx[i] * bx[i] + by[i] * by[i] + bz[i] * bz[i])
-                ea  = np.cross(ep, r) / np.linalg.norm(np.cross(ep, r))
-                er  = np.cross(ea, ep) / np.linalg.norm(np.cross(ea, ep))
+                #ea  = np.cross(ep, r) / np.linalg.norm(np.cross(ep, r))
+                #er  = np.cross(ea, ep) / np.linalg.norm(np.cross(ea, ep))
+                ea  = crossD(ep, r) / normD(crossD(ep, r))
+                er  = crossD(ea, ep) / normD(crossD(ea, ep))
                 # apply rotation for B
-                tempFields['bp'][i] = (ep[0] * bx[i]) + (ep[1] * by[i]) + (ep[2] * bz[i])
-                tempFields['ba'][i] = (ea[0] * bx[i]) + (ea[1] * by[i]) + (ea[2] * bz[i])
-                tempFields['br'][i] = (er[0] * bx[i]) + (er[1] * by[i]) + (er[2] * bz[i])
+                tempFields[i][0] = (ep[0] * bx[i]) + (ep[1] * by[i]) + (ep[2] * bz[i])
+                tempFields[i][1] = (ea[0] * bx[i]) + (ea[1] * by[i]) + (ea[2] * bz[i])
+                tempFields[i][2] = (er[0] * bx[i]) + (er[1] * by[i]) + (er[2] * bz[i])
                 # Apply the rotation for vector V1
-                tempFields['v1p'][i] = (ep[0] * v1x[i]) + (ep[1] * v1y[i]) + (ep[2] * v1z[i])
-                tempFields['v1a'][i] = (ea[0] * v1x[i]) + (ea[1] * v1y[i]) + (ea[2] * v1z[i])
-                tempFields['v1r'][i] = (er[0] * v1x[i]) + (er[1] * v1y[i]) + (er[2] * v1z[i])
+                tempFields[i][3] = (ep[0] * v1x[i]) + (ep[1] * v1y[i]) + (ep[2] * v1z[i])
+                tempFields[i][4] = (ea[0] * v1x[i]) + (ea[1] * v1y[i]) + (ea[2] * v1z[i])
+                tempFields[i][5] = (er[0] * v1x[i]) + (er[1] * v1y[i]) + (er[2] * v1z[i])
                 # testing whether the rotation is correct
-                tempFields['b_fac'][i] = np.linalg.norm([tempFields['bp'][i], tempFields['ba'][i], tempFields['br'][i]])
-                tempFields['b_orig'][i] = np.linalg.norm([bx[i], by[i], bz[i]])
+                tempFields[i][6] = normD([tempFields[i][0], tempFields[i][1], tempFields[i][2]])
+                tempFields[i][7] = normD([bx[i], by[i], bz[i]])
 
-            self.fields.append(tempFields)
+
+            tempFieldsDF = pd.DataFrame(tempFields,columns=['bp', 'ba', 'br', 'v1p', 'v1a', 'v1r', 'b_fac', 'b_orig'])
+            self.fields.append(tempFieldsDF) #se pegarmos muitos tempos precisamos mudar isso
+
+            
         return (self.fields)
 
 
@@ -108,15 +133,19 @@ class DifCoeficient(object):
     def integPhi(self,component,m):
         self.resultA = []
         self.resultB = []
-        for k in range(len(self.fields)):
-            a = np.zeros((len(self.fields[k][component])))
-            b = np.zeros((len(self.fields[k][component])))
-            for i in range(len(self.inputArray[k]['phi'])):
-                a[i] = self.fields[k][component].values[i] * np.sin(m * self.inputArray[k]['phi'].values[i])
-                b[i] = self.fields[k][component].values[i] * np.cos(m * self.inputArray[k]['phi'].values[i])
 
-            self.resultA.append(integrate.simps(a,self.inputArray[k]['phi'].values))
-            self.resultB.append(integrate.simps(b,self.inputArray[k]['phi'].values))
+        for k in range(len(self.fields)):
+            field = self.fields[k][component].values
+            angle = self.inputArray[k]['phi'].values
+            angleSize = len(self.inputArray[k]['phi'])
+            a = np.zeros(angleSize)
+            b = a
+            for i in range(angleSize):
+                a[i] = field[i] * np.sin(m * angle[i])
+                b[i] = field[i] * np.cos(m * angle[i])
+
+            self.resultA.append(integrate.simps(a,angle))
+            self.resultB.append(integrate.simps(b,angle))
 
         return (self.resultA, self.resultB)
 
@@ -146,7 +175,7 @@ class CoeffAnalysis():
         fa, ta, Sxxa = [0]*self.a.shape[0], [0]*self.a.shape[0], [0]*self.a.shape[0]
         self.intePot = [0]*self.a.shape[0]
         for i in range(0,self.a.shape[0]):
-            fa[i], ta[i], Sxxa[i] = signal.spectrogram(self.a[i,:], 1./30, window='hamming',nperseg=15, nfft=256)
+            fa[i], ta[i], Sxxa[i] = signal.spectrogram(self.b[i,:], 1./30, window='hamming',nperseg=15, nfft=256)
             inPot = np.zeros((Sxxa[i].shape[1]))
             for k in range(0,Sxxa[i].shape[1]):
                 inPot[k] = sum(Sxxa[i][k])
@@ -175,35 +204,45 @@ class CoeffAnalysis():
 ######################
 # testes
 #####################33
-filename = 'results_slc3/*.txt'
-# 	sizePhi = 720
+filename = 'results_slc/*.txt'
+#   sizePhi = 720
 #
 #
-# 	iEx = 0
-# 	iEy = 1
-# 	iEz = 2
-# 	iBx = 3
-# 	iBy = 4
-# 	iBz = 5
+#   iEx = 0
+#   iEy = 1
+#   iEz = 2
+#   iBx = 3
+#   iBy = 4
+#   iBz = 5
 #
 #     dC = DifCoeficient(filename)
+time0 = time.time()
 dC = DifCoeficient(filename, RVal=4.5, TimeStep = 30)
-dC.read()
-
+time1 = time.time()
+print time1-time0
+dC.read() 
+time2 = time.time()
+print time2-time1
 aa = dC.rotate_field_fac()
-
+time3 = time.time()
+print time3-time2
 bb = []
 for m in range(0,45):
     bb.append(dC.integPhi('v1a', m))
-
+time4 = time.time()
+print time4-time3
 coef = CoeffAnalysis(bb)
-
+time5 = time.time()
+print time5-time4
 teat = coef.timeOrg()
-
-
+time6= time.time()
+print time6-time5
 dd = coef.psdM()
-
+time7 = time.time()
+print time7-time6
 coef.plots()
+time8 = time.time()
+print time8-time7
 
 
 
@@ -221,9 +260,17 @@ coef.plots()
 
 
 
-
-
-
+##test marcos
+#tempFields['bp'][i] = (ep[0] * bx[i]) + (ep[1] * by[i]) + (ep[2] * bz[i])
+#                tempFields['ba'][i] = (ea[0] * bx[i]) + (ea[1] * by[i]) + (ea[2] * bz[i])
+#                tempFields['br'][i] = (er[0] * bx[i]) + (er[1] * by[i]) + (er[2] * bz[i])
+#                # Apply the rotation for vector V1
+#                tempFields['v1p'][i] = (ep[0] * v1x[i]) + (ep[1] * v1y[i]) + (ep[2] * v1z[i])
+#                tempFields['v1a'][i] = (ea[0] * v1x[i]) + (ea[1] * v1y[i]) + (ea[2] * v1z[i])
+#                tempFields['v1r'][i] = (er[0] * v1x[i]) + (er[1] * v1y[i]) + (er[2] * v1z[i])
+#                # testing whether the rotation is correct
+#                tempFields['b_fac'][i] = normD([tempFields['bp'][i], tempFields['ba'][i], tempFields['br'][i]])
+#                tempFields['b_orig'][i] = normD([bx[i], by[i], bz[i]])
 
 
 
@@ -279,33 +326,33 @@ coef.plots()
     #
     #
     # def coordTransform(data,phi):
-    # 	#transformation matrix
-    # 	A = np.zeros((3,3))
+    #   #transformation matrix
+    #   A = np.zeros((3,3))
     #
-    # 	E = np.zeros((len(phi),3))
+    #   E = np.zeros((len(phi),3))
     #
-    # 	for i in range(len(phi)):
-    # 		phi_temp = phi[i]
-    # 		#A[linha,coluna]
-    # 		A[0,0] = np.cos(phi_temp)
-    # 		A[0,1] = np.sin(phi_temp)
-    # 		A[0,2] = 0.
+    #   for i in range(len(phi)):
+    #       phi_temp = phi[i]
+    #       #A[linha,coluna]
+    #       A[0,0] = np.cos(phi_temp)
+    #       A[0,1] = np.sin(phi_temp)
+    #       A[0,2] = 0.
     #
-    # 		A[1,0] = -np.sin(phi_temp)
-    # 		A[1,1] = np.cos(phi_temp)
-    # 		A[1,2] = 0.
+    #       A[1,0] = -np.sin(phi_temp)
+    #       A[1,1] = np.cos(phi_temp)
+    #       A[1,2] = 0.
     #
-    # 		A[2,0] = 0.
-    # 		A[2,1] = 0.
-    # 		A[2,2] = 1.
+    #       A[2,0] = 0.
+    #       A[2,1] = 0.
+    #       A[2,2] = 1.
     #
-    # 		for l in range(3):
-    # 			temp = 0
-    # 			for k in range(3):
-    # 				temp += A[l,k] * data[i,k]
-    # 			E[i,l] = temp
+    #       for l in range(3):
+    #           temp = 0
+    #           for k in range(3):
+    #               temp += A[l,k] * data[i,k]
+    #           E[i,l] = temp
     #
-    # 	return E
+    #   return E
     #
     #
     #
@@ -476,7 +523,7 @@ coef.plots()
     #     #
     # fields = rotate_field_fac(inpArray[0])
     #
-	# inpArray = main()
+    # inpArray = main()
 
 # plt.plot(Serie[22][1])
 # plt.plot(Electric[:,1])
